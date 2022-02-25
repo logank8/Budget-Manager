5d95aa9611ac65a7b094a5832551e820686b420a
8bitlogan <lokeener@student.ubc.ca>
2022-02-25 13:17:07 -0800

    editing stuff condensed in ui (range)

--- src/main/ui/BudgetApp.java
+++ src/main/ui/BudgetApp.java
@@ -44,44 +44,81 @@ public class BudgetApp {
                 processCommand(command);
             }
         }
+        System.out.println("Goodbye!");
     }
 
     // MODIFIES: this
     // EFFECTS: processes user command
     private void processCommand(String command) {
-        if ("i".equals(command)) {
+        if ("i".equals(command)) { // edit income
             changeIncome();
-        } else if ("c".equals(command)) {
+        } else if ("x".equals(command)) { // edit category
             chooseCategory(0);
-        } else if ("n".equals(command)) {
-            chooseCategory(1);
-        } else if ("s".equals(command)) {
+        } else if ("s".equals(command)) { // show savings
             System.out.println("Savings: " + currentBudget.getSavings());
-        } else if ("a".equals(command)) {
+        } else if ("a".equals(command)) { // add amount
             newCategory(0);
-        } else if ("r".equals(command)) {
+        } else if ("r".equals(command)) { // add range
             newCategory(1);
-        } else if ("e".equals(command)) {
-            chooseCategory(2);
-        } else if ("f".equals(command)) {
+        } else if ("e".equals(command)) { // remove category
+            chooseCategory(1);
+        } else if ("f".equals(command)) { // save
             saveBudget();
-        } else if ("l".equals(command)) {
+        } else if ("l".equals(command)) { // load
             loadBudget();
         } else {
             System.out.println("Selection not valid...");
         }
     }
 
+    // MODIFIES: currentBudget income
+    // EFFECTS: adds user int input to income & outputs new income value
+    private void changeIncome() {
+        System.out.println("Please enter a value to add to income");
+        int answer = input.nextInt();
+        if ((currentBudget.getIncome() + answer) < 0) {
+            System.out.println("Answer not valid");
+            changeIncome();
+        } else {
+            currentBudget.addIncome(answer);
+            System.out.println("New income: " + currentBudget.getIncome());
+        }
+    }
+
+    // EFFECTS: processes user input for category selection; then:
+    // nextAction 0 = editCategory
+    // nextAction 1 = removeCategory
+    private void chooseCategory(int nextAction) {
+        if (currentBudget.getCategories().isEmpty()) {
+            System.out.println("No categories to choose from.");
+        } else {
+            Category subject;
+            System.out.println("Enter category selection:");
+            List<String> names = allNames();
+            String answer = input.next().toLowerCase();
+            if (names.contains(answer)) {
+                subject = currentBudget.getCategories().get(names.indexOf(answer));
+                if (nextAction == 0) {
+                    editCategory(subject);
+                } else {
+                    removeCategory(subject);
+                }
+            } else {
+                System.out.println("Answer not valid");
+                chooseCategory(nextAction);
+            }
+        }
+    }
+
+
     // EFFECTS: processes user input for title for new category
     // nextAction 0 = new amount
     // nextAction 1 = new range
     private void newCategory(int nextAction) {
-        System.out.println("Please enter a title for the new category");
-        String answer = input.next();
         if (nextAction == 0) {
-            addAmount(answer);
+            addAmount(" the amount");
         } else {
-            addRange(answer);
+            addRange("the range");
         }
     }
 
@@ -103,33 +140,6 @@ public class BudgetApp {
         currentBudget.addAmount(name, answer);
     }
 
-    // EFFECTS: processes user input for category selection; then:
-    // nextAction 0 = editCategory
-    // nextAction 1 = nameCategory
-    // nextAction 2 = removeCategory
-    private void chooseCategory(int nextAction) {
-        if (currentBudget.getCategories().isEmpty()) {
-            System.out.println("No categories to choose from.");
-        } else {
-            Category subject;
-            System.out.println("Enter category selection:");
-            List<String> names = allNames();
-            String answer = input.next().toLowerCase();
-            if (names.contains(answer)) {
-                subject = currentBudget.getCategories().get(names.indexOf(answer));
-                if (nextAction == 0) {
-                    editCategory(subject);
-                } else if (nextAction == 1) {
-                    nameCategory(subject);
-                } else {
-                    removeCategory(subject);
-                }
-            } else {
-                System.out.println("Answer not valid");
-                chooseCategory(nextAction);
-            }
-        }
-    }
 
     // REQUIRES: object category not null
     // MODIFIES: this
@@ -144,33 +154,6 @@ public class BudgetApp {
             Amount amount = currentBudget.getAmounts().get(amountNames().indexOf(categoryName));
             currentBudget.removeAmount(amount);
         }
-    }
-
-    // EFFECTS: creates corresponding list of titles of every category
-    private List<String> allNames() {
-        List<String> names = new ArrayList<>();
-        for (Category c : currentBudget.getCategories()) {
-            names.add(c.getTitle().toLowerCase());
-        }
-        return names;
-    }
-
-    // EFFECTS: creates corresponding list of titles of every range
-    private List<String> rangeNames() {
-        List<String> names = new ArrayList<>();
-        for (Range r : currentBudget.getRanges()) {
-            names.add(r.getTitle().toLowerCase());
-        }
-        return names;
-    }
-
-    // EFFECTS: creates corresponding list of titles of every amount
-    private List<String> amountNames() {
-        List<String> names = new ArrayList<>();
-        for (Amount a : currentBudget.getAmounts()) {
-            names.add(a.getTitle().toLowerCase());
-        }
-        return names;
     }
 
     // REQUIRES: category object not null
@@ -203,6 +186,9 @@ public class BudgetApp {
     // MODIFIES: range
     // EFFECTS: processes user input for new lower & upper bounds for range
     private void editRange(Range range) {
+        System.out.println("Enter a new name for " + range.getTitle());
+        String newName = input.next();
+        range.setTitle(newName);
         System.out.println("Enter a lower bound for " + range.getTitle());
         int answer = input.nextInt();
         if (answer >= 0) {
@@ -238,19 +224,7 @@ public class BudgetApp {
         }
     }
 
-    // MODIFIES: income
-    // EFFECTS: adds user int input to income & outputs new income value
-    private void changeIncome() {
-        System.out.println("Please enter a value to add to income");
-        int answer = input.nextInt();
-        if ((currentBudget.getIncome() + answer) < 0) {
-            System.out.println("Answer not valid");
-            changeIncome();
-        } else {
-            currentBudget.addIncome(answer);
-            System.out.println("New income: " + currentBudget.getIncome());
-        }
-    }
+
 
     private void saveBudget() {
         try {
@@ -277,8 +251,7 @@ public class BudgetApp {
         showInfo();
         System.out.println("Controls: ");
         System.out.println("i: edit income");
-        System.out.println("c: edit a spending category amount"); // condense this
-        System.out.println("n: edit a spending category name");
+        System.out.println("x: edit a spending category");
         System.out.println("r: add a new spending category (range)"); // condense
         System.out.println("a: add a new spending category (definite amount)");
         System.out.println("e: remove a spending category");
@@ -304,6 +277,33 @@ public class BudgetApp {
                 System.out.println(a.getTitle() + ": " + a.getAmount());
             }
         }
+    }
+
+    // EFFECTS: creates corresponding list of titles of every category
+    private List<String> allNames() {
+        List<String> names = new ArrayList<>();
+        for (Category c : currentBudget.getCategories()) {
+            names.add(c.getTitle().toLowerCase());
+        }
+        return names;
+    }
+
+    // EFFECTS: creates corresponding list of titles of every range
+    private List<String> rangeNames() {
+        List<String> names = new ArrayList<>();
+        for (Range r : currentBudget.getRanges()) {
+            names.add(r.getTitle().toLowerCase());
+        }
+        return names;
+    }
+
+    // EFFECTS: creates corresponding list of titles of every amount
+    private List<String> amountNames() {
+        List<String> names = new ArrayList<>();
+        for (Amount a : currentBudget.getAmounts()) {
+            names.add(a.getTitle().toLowerCase());
+        }
+        return names;
     }
 
     // EFFECTS: initializes budget and scanner
