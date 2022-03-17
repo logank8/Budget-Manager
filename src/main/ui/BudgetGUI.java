ceb13df115149f85b79965ce4f64f10b8ca6c8a8
8bitlogan <lokeener@student.ubc.ca>
2022-03-17 00:43:28 -0700

    save & load implemented

--- src/main/ui/BudgetGUI.java
+++ src/main/ui/BudgetGUI.java
@@ -1,37 +1,48 @@
 package ui;
 
 import model.Budget;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
 
-public class BudgetGUI extends JFrame implements MouseListener {
+public class BudgetGUI extends JFrame {
+
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
+
     private Budget budget;
-    private JDesktopPane desktop;
     private JLabel income;
-    private JInternalFrame infoPanel;
-    private JMenuBar menuBar;
-    private JMenu fileHandler;
+    private final JInternalFrame infoPanel;
+    private final JMenu fileHandler;
+
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    public static final String JSON_STORE = "./data/budget.json";
 
     public BudgetGUI() {
         budget = new Budget();
-        desktop = new JDesktopPane();
+        JDesktopPane desktop = new JDesktopPane();
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
 
         setContentPane(desktop);
         setTitle("CPSC 210: Budget Manager");
         setSize(WIDTH, HEIGHT);
 
-        menuBar = new JMenuBar();
+        JMenuBar menuBar = new JMenuBar();
         fileHandler = new JMenu("File...");
-        addSave();
+        addSaveAndLoad();
         menuBar.add(fileHandler);
 
 
@@ -50,9 +61,15 @@ public class BudgetGUI extends JFrame implements MouseListener {
         setVisible(true);
     }
 
-    private void addSave() {
-        JMenuItem save = new JMenuItem("Save budget");
+    private void addSaveAndLoad() {
+        JMenuItem save = new JMenuItem("Save");
+        save.getAccessibleContext().setAccessibleDescription("Save budget to file");
+        save.addActionListener(new SaveAction());
+        JMenuItem load = new JMenuItem("Load");
+        load.getAccessibleContext().setAccessibleDescription("Load budget from file");
+        load.addActionListener(new LoadAction());
         fileHandler.add(save);
+        fileHandler.add(load);
     }
 
 
@@ -61,6 +78,10 @@ public class BudgetGUI extends JFrame implements MouseListener {
         int width = Toolkit.getDefaultToolkit().getScreenSize().width;
         int height = Toolkit.getDefaultToolkit().getScreenSize().height;
         setLocation((width - getWidth()) / 2, (height - getHeight()) / 2);
+    }
+
+    private void displayUpdate() {
+        income.setText("Income: " + budget.getIncome());
     }
 
     private void addIncome() {
@@ -78,6 +99,30 @@ public class BudgetGUI extends JFrame implements MouseListener {
         infoPanel.add(editButton);
     }
 
+    private void saveBudget() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(budget);
+            jsonWriter.close();
+            JOptionPane.showMessageDialog(null, "Saved file");
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "ERROR: file not found");
+        }
+    }
+
+    // MODIFIES: budget
+    // EFFECTS: loads budget from json
+    private void loadBudget() {
+        try {
+            budget = jsonReader.read();
+            displayUpdate();
+            JOptionPane.showMessageDialog(null, "Loaded current budget from " + JSON_STORE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to read from file " + JSON_STORE);
+        }
+    }
+
+
     private class EditIncomeAction extends AbstractAction {
 
         public EditIncomeAction() {
@@ -88,39 +133,39 @@ public class BudgetGUI extends JFrame implements MouseListener {
         public void actionPerformed(ActionEvent e) {
             String input = JOptionPane.showInputDialog(new JFormattedTextField(),
                     "Please input new value for income");
-            if (!input.isEmpty()) {
+            if (input != null) {
                 try {
                     budget.setIncome(parseInt(input));
                     income.setText("Income: " + budget.getIncome());
-                } catch (NumberFormatException exception) {
+                } catch (NumberFormatException e2) {
                     JOptionPane.showMessageDialog(null, "ERROR: Integer value expected");
                 }
+
             }
         }
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("Click");
+    private class SaveAction extends AbstractAction {
+
+        public SaveAction() {
+            super("Save");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveBudget();
+        }
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
+    private class LoadAction extends AbstractAction {
 
-    }
+        public LoadAction() {
+            super("Load");
+        }
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            loadBudget();
+        }
     }
 }
